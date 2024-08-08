9e42c67fac7e737a5c49881436fa84c03215beec
quaksilver <160717350+quaksilver@users.noreply.github.com>
2024-08-08 21:23:24 +0200

    starting block and monomer classes

---
+++ block.java
@@ -0,0 +1,36 @@
+public class block {
+    float positionX, positionY;
+    int levelEPS = 0;
+    boolean occupied = false;
+
+    public block(float positionX, float positionY, int levelEPS, boolean occupied) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.levelEPS = levelEPS;
+        this.occupied = occupied;
+
+    }
+
+    private void setEPS(int level){
+        this.levelEPS = level;
+    }
+
+    private void setPositon (float x, float y){
+        this.positionX = x;
+        this.positionY = y;
+    } 
+
+    private void setOccupied (boolean value){
+        this.occupied = value;
+    }
+
+    // need a getter for position, can't decide if we should do one for x and one for y or if together what type
+
+    private int getLevelEPS (){
+        return levelEPS;
+    }
+
+    private boolean occupied (){
+        return occupied;
+    }
+}
---
+++ monomer.java
@@ -0,0 +1,44 @@
+public class monomer {
+
+    block position;
+    String type;
+    char colour;
+    // linked list of monomers it is bonded to?
+
+    public monomer(block position,String type,char colour) {
+        this.position = position;
+        this.type = type;
+        this.colour = colour;
+
+    }
+
+    private void bond (monomer other){
+        // add other to linked list of bonds
+
+    }
+
+    private void setPositon (block pos){
+        this.position = pos;
+    }
+
+    private void setType (String type){
+        this.type = type;
+    }
+
+    private void setColour (char colour){
+        this.colour = colour;
+    }
+
+    private block getPosition (){
+        return this.position;
+    }
+
+    private String getType (){
+        return this.type;
+    }
+
+    private char getColour (){
+        return this.colour;
+    }
+
+}
